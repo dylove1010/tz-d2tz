ee17fde7d0a2abd74fa546ea7f12d7b1f2c8d7af
dylove1010 <dylove1010@foxmail.com>
2025-09-15 02:03:12 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Installing Playwright browsers..."
-PLAYWRIGHT_BROWSERS_PATH=$HOME/.cache/ms-playwright playwright install chromium
-echo "PLAYWRIGHT_CHROMIUM_PATH=$HOME/.cache/ms-playwright/chromium-*/chrome-linux/chromium" >> $HOME/.env
-pip install -r requirements.txt
